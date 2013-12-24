b69fcee01e5eba72e6be8a054062d044f37c9bbe
Jehan-Guillaume de Rorthais <jgdr@dalibo.com>
2013-12-24 12:05:49 +0100

    Removes links to graphs from the server page
    On each server page, we used to show services and graphs for each
    services, which makes the page and workflow difficult to understand.
    This patch removes graphs links from the server page. These links will
    be available from the services pages next to their corresponding graph
    in a futur patch.

--- ui/lib/OPM/Server.pm
+++ ui/lib/OPM/Server.pm
@@ -78,13 +78,20 @@ sub host {
     my $dbh  = $self->database();
     my $id   = $self->param('id');
     my $sql;
+    my $row;
+    my $hostname;
+    my @services;
 
-    $sql = $dbh->prepare("SELECT COUNT(*) FROM public.list_servers() WHERE id = ?");
+    $sql = $dbh->prepare(q{
+        SELECT hostname
+        FROM public.list_servers()
+        WHERE id = ?
+    });
     $sql->execute($id);
-    my $found = ( $sql->fetchrow() == 1 );
+    $hostname = $sql->fetchrow();
     $sql->finish();
 
-    if (! $found){
+    if ( not $hostname ){
         $dbh->disconnect();
         $self->stash(
             message => 'Server not found',
@@ -93,52 +100,30 @@ sub host {
         return $self->render_not_found;
     }
 
-    # FIXME: handle pr_grapher dependancy
+    # create missing graphs for given server
+    # FIXME: handle error
     $sql = $dbh->prepare("SELECT pr_grapher.create_graph_for_wh_nagios(?)");
     $sql->execute($id);
     $sql->finish();
 
-    # FIXME: handle pr_grapher and wh_nagios dependancy
-    $sql = $dbh->prepare(
-        "SELECT s.id AS id_service, s.service, lower(s.state) as state,
-            lg.id AS id_graph, lg.graph
+    # Fetch all services for the given server
+    $sql = $dbh->prepare(q{
+        SELECT s.id AS id_service, s.service, lower(s.state) as state 
         FROM wh_nagios.list_services() s
-        JOIN pr_grapher.list_wh_nagios_graphs() lg
-            ON lg.id_service = s.id
         WHERE s.id_server = ?
-        ORDER BY s.service, s.id;
-        "
+        ORDER BY s.service, s.id
+    });
+    # FIXME: handle error
+    $sql->execute($id);
+
+    push @services, $row while $row = $sql->fetchrow_hashref();
+    $sql->finish();
+
+    $self->stash(
+        services => \@services,
+        hostname => $hostname,
+        id => $id
     );
-    $sql->execute($id);
-
-    my $curr_service = { 'id' => -1 };
-    my @services;
-    while ( my $row = $sql->fetchrow_hashref() ) {
-        if ( $curr_service->{'id'} != $row->{'id_service'} ) {
-            push @services, \%{ $curr_service } if $curr_service->{'id'} != -1;
-            $curr_service = {
-                'id'          => $row->{'id_service'},
-                'service'     => $row->{'service'},
-                'state'       => $row->{'state'},
-                'graphs'      => []
-            };
-        }
-
-        push @{ $curr_service->{'graphs'} }, {
-            'id_graph' => $row->{'id_graph'},
-            'graph'    => $row->{'graph'}
-        };
-    }
-    push @services, \%{ $curr_service } if $curr_service->{'id'} != -1;
-    $sql->finish();
-
-    $sql = $dbh->prepare(
-        "SELECT hostname FROM public.list_servers() WHERE id = ?");
-    $sql->execute($id);
-    my $hostname = $sql->fetchrow();
-    $sql->finish();
-
-    $self->stash( services => \@services, hostname => $hostname, id => $id );
 
     $dbh->disconnect();
     $self->render();
