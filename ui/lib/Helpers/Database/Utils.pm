0a852c1d9063ac500108f9f6757b64b5bed9a197
Ronan Dunklau <ronan.dunklau@dalibo.com>
2014-02-11 15:02:34 +0100

    Fix push to array_ref.
    This was not supported in perl<=5.12

--- ui/lib/Helpers/Database/Utils.pm
+++ ui/lib/Helpers/Database/Utils.pm
@@ -7,37 +7,36 @@ package Helpers::Database::Utils;
 
 use Mojo::Base -base;
 
-has db => sub {};
-has schema => sub { return "public" };
-has connection => sub {  };
-has is_from => sub { 0 };
+has db         => sub { };
+has schema     => sub { return "public" };
+has connection => sub { };
+has is_from    => sub {0};
 
 sub AUTOLOAD {
     my $self = shift,
-    my ($package, $method) = split /::(\w+)$/, our $AUTOLOAD;
-    my $schema = $self->schema;
+        my ( $package, $method ) = split /::(\w+)$/, our $AUTOLOAD;
+    my $schema       = $self->schema;
     my $qualified_fn = "$schema.$method";
-    return $self->db->db_sub_one($self->connection, $qualified_fn, @_);
+    return $self->db->db_sub_one( $self->connection, $qualified_fn, @_ );
 }
 
 sub DESTROY {
-};
+}
 
 sub groupby {
-    my ($mod, $values, $sub) = @_;
-    my $results = {};
+    my ( $mod, $values, $sub ) = @_;
+    my $results  = {};
     my $last_key = "";
     my $current_values;
-    foreach my $value (@{$values}) {
-      $_ = $value;
-      my $key = &$sub(%{$value});
-      if($key ne $last_key)
-      {
-        $last_key = $key;
-        $current_values = [];
-        $results->{$key} = $current_values;
-      }
-      push $current_values, $value;
+    foreach my $value ( @{$values} ) {
+        $_ = $value;
+        my $key = &$sub( %{$value} );
+        if ( $key ne $last_key ) {
+            $last_key        = $key;
+            $current_values  = [];
+            $results->{$key} = $current_values;
+        }
+        push @$current_values, $value;
     }
     return $results;
 }
