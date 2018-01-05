b0d084c3bef52faea47440878b9d714de445f28d
Julien Rouhaud <julien.rouhaud@free.fr>
2018-01-05 10:00:37 +0100

    Update copyright year

--- ui/lib/Helpers/Permissions.pm
+++ ui/lib/Helpers/Permissions.pm
@@ -3,7 +3,7 @@ package Helpers::Permissions;
 # This program is open source, licensed under the PostgreSQL License.
 # For license terms, see the LICENSE file.
 #
-# Copyright (C) 2012-2017: Open PostgreSQL Monitoring Development Group
+# Copyright (C) 2012-2018: Open PostgreSQL Monitoring Development Group
 
 use Mojo::Base 'Mojolicious::Plugin';
 
